a932020a31aff26179289724f5402137c27d2f93
coczero <timstadler905@googlemail.com>
2015-04-28 10:27:24 +0200

    Renamed Form1 to MainForm, create LastfmClient

---
+++ Scrubbler/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IF.Lastfm.Core.Api;
+using IF.Lastfm.Core.Objects;
+
+namespace Scrubbler
+{
+  public partial class MainForm : Form
+  {
+    private LastfmClient _client;
+    private const string APIKEY = "a3629265939bfe7b7e631768118933bd";
+    private const string APISECRET = "9857e9c9916a5e2c327a160d1d49d457";
+
+    /// <summary>
+    /// Constructor. Creates the client.
+    /// </summary>
+    public MainForm()
+    {
+      InitializeComponent();
+      _client = new LastfmClient(APIKEY, APISECRET);
+    }
+  }
+}
--- Scrubbler/Program.cs
+++ Scrubbler/Program.cs
@@ -16,7 +16,7 @@ namespace Scrubbler
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Form1());
+      Application.Run(new MainForm());
     }
   }
 }
